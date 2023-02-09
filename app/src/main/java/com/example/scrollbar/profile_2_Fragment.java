927417d6d06e8b127b347af90dfc90eb3db16deb
yabsera seyfe <113304237+yabse@users.noreply.github.com>
2023-02-09 10:07:17 +0200

    finished project

---
+++ app/src/main/java/com/example/scrollbar/profile_2_Fragment.java
@@ -0,0 +1,20 @@
+package com.example.scrollbar;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class profile_2_Fragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_profile_2_, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/scrollbar/splash.java
@@ -0,0 +1,28 @@
+package com.example.scrollbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(splash.this, MainActivity.class));
+                finish();
+            }
+        },4000);
+    }
+}
